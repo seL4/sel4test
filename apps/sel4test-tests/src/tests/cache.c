eee0de78bc89693aab7cfcc0b8d6e1b7eee87369
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-02-23 13:36:58 +1100

    Test that cache operations can be performed on all page sizes

--- apps/sel4test-tests/src/tests/cache.c
+++ apps/sel4test-tests/src/tests/cache.c
@@ -17,6 +17,7 @@
 #include <sel4utils/util.h>
 
 #include "../helpers.h"
+#include "frame_type.h"
 
 #if defined(CONFIG_ARCH_ARM) && defined(CONFIG_HAVE_CACHE)
 static int
@@ -107,6 +108,73 @@ test_page_flush(env_t env, void *args)
 }
 
 static int
+test_large_page_flush_operation(env_t env, void *args)
+{
+    int num_frame_types = ARRAY_SIZE(frame_types);
+    seL4_CPtr frames[num_frame_types];
+    int error;
+    vka_t *vka = &env->vka;
+    bool pt_mapped = false;
+
+    /* Grab some free vspace big enough to hold all the tests. */
+    seL4_Word vstart;
+    reservation_t reserve = vspace_reserve_range(&env->vspace, 2 * (1 << 25),
+                                                  seL4_AllRights, 1, (void **) &vstart);
+    test_assert(reserve.res != 0);
+    vstart = ALIGN_UP(vstart, (1 << 25));
+
+    /* Create us some frames to play with. */
+    for (int i = 0; i < num_frame_types; i++) {
+        frames[i] = vka_alloc_frame_leaky(vka, CTZ(frame_types[i].size));
+        assert(frames[i]);
+    }
+
+    /* Also create a pagetable to map the pages into. */
+    seL4_CPtr pt = vka_alloc_page_table_leaky(vka);
+
+    /* Map the pages in. */
+    for (int i = 0; i < num_frame_types; i++) {
+        if (frame_types[i].need_pt && !pt_mapped) {
+            /* Map the pagetable in. */
+            error = seL4_ARCH_PageTable_Map(pt, env->page_directory,
+                                            vstart + frame_types[i].vaddr_offset,
+                                            seL4_ARCH_Default_VMAttributes);
+            test_assert(error == 0);
+
+            pt_mapped = true;
+        }
+
+        error = seL4_ARCH_Page_Map(frames[i], env->page_directory,
+                                   vstart + frame_types[i].vaddr_offset, seL4_AllRights,
+                                   seL4_ARCH_Default_VMAttributes);
+        test_assert(error == 0);
+    }
+
+    /* See if we can invoke page flush on each of them */
+    for (int i = 0; i < num_frame_types; i++) {
+        error = seL4_ARM_Page_Invalidate_Data(frames[i], 0, frame_types[i].size);
+        test_assert(error == 0);
+        error = seL4_ARM_Page_Clean_Data(frames[i], 0, frame_types[i].size);
+        test_assert(error == 0);
+        error = seL4_ARM_Page_CleanInvalidate_Data(frames[i], 0, frame_types[i].size);
+        test_assert(error == 0);
+        error = seL4_ARM_Page_Unify_Instruction(frames[i], 0, frame_types[i].size);
+        test_assert(error == 0);
+        error = seL4_ARM_PageDirectory_Invalidate_Data(env->page_directory, vstart + frame_types[i].vaddr_offset, vstart + frame_types[i].vaddr_offset + frame_types[i].size);
+        test_assert(error == 0);
+        error = seL4_ARM_PageDirectory_Clean_Data(env->page_directory, vstart + frame_types[i].vaddr_offset, vstart + frame_types[i].vaddr_offset + frame_types[i].size);
+        test_assert(error == 0);
+        error = seL4_ARM_PageDirectory_CleanInvalidate_Data(env->page_directory, vstart + frame_types[i].vaddr_offset, vstart + frame_types[i].vaddr_offset + frame_types[i].size);
+        test_assert(error == 0);
+        error = seL4_ARM_PageDirectory_Unify_Instruction(env->page_directory, vstart + frame_types[i].vaddr_offset, vstart + frame_types[i].vaddr_offset + frame_types[i].size);
+        test_assert(error == 0);
+    }
+
+    return SUCCESS;
+}
+
+
+static int
 test_page_directory_flush(env_t env, void *args)
 {
     seL4_CPtr frame, framec;
@@ -191,10 +259,8 @@ test_page_directory_flush(env_t env, void *args)
     return SUCCESS;
 }
 
-
-
-
 DEFINE_TEST(CACHEFLUSH0001, "Test a cache maintenance on pages", test_page_flush)
 DEFINE_TEST(CACHEFLUSH0002, "Test a cache maintenance on page directories", test_page_directory_flush)
+DEFINE_TEST(CACHEFLUSH0003, "Test that cache maintenance can be done on large pages", test_large_page_flush_operation)
 
 #endif
