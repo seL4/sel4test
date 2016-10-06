seL4/sel4test

commit 35944e1e613f3806d942433fc432d6cf7d89b454

User device untyped and s/PIT/HPET for x86
This commit changes the way sel4test-driver passes
device memory to sel4test-tests: instead of copying the frame
caps it copies untypeds, to be compataible with how seL4_libs
has changed after device untyped. sel4test-tests now gives
the relevant untyped to its allocator so seL4_libs can
initialise the relevant devices.

Additionally, this commit changes to use the HPET and IOAPIC
instead of the legacy PIT/PIC on x86, allowing us much greater
timer accuracy in tests and faster tests in general.

apps/sel4test-driver/src/arch/arm/arch.c
@@ -10,57 +10,36 @@
 
 #include "../../test.h"
 
+#include <sel4platsupport/device.h>
 #include <sel4platsupport/plat/timer.h>
 #include <sel4platsupport/plat/serial.h>
 
 void
 arch_init_timer_caps(env_t env)
 {
-    /* get the timer frame cap */
-    seL4_CPtr cap;
-    seL4_Word cookie;
-    int error = vka_cspace_alloc(&env->vka, &cap);
-    if (error) {
-        ZF_LOGF("Failed to allocate cslot for timer frame cap path");
-    }
-
-    vka_cspace_make_path(&env->vka, cap, &env->frame_path);
-    error = vka_utspace_alloc_at(&env->vka, &env->frame_path, kobject_get_type(KOBJECT_FRAME, PAGE_BITS_4K), PAGE_BITS_4K, DEFAULT_TIMER_PADDR, &cookie);
-    if (error) {
-        ZF_LOGF("Failed to get frame cap for %p\n", (void *) DEFAULT_TIMER_PADDR);
-    }
+    int error = sel4platsupport_copy_irq_cap(&env->vka, &env->simple, DEFAULT_TIMER_INTERRUPT,
+            &env->irq_path);
+    ZF_LOGF_IF(error, "Failed to copy irq cap");
 }
 
 void
 arch_copy_timer_caps(test_init_data_t *init, env_t env, sel4utils_process_t *test_process)
 {
-
-    /* Timer frame cap (only for arm). Here we assume the sel4platsupport
-     * default timer only requires one frame. */
-    init->timer_frame = copy_cap_to_process(test_process, env->frame_path.capPtr);
-    if (init->timer_frame == 0) {
-        ZF_LOGF("Failed to copy timer frame cap to process");
-    }
+    /* nothing to do */
 }
 
 int
 arch_init_serial_caps(env_t env)
 {
     /* Get the serial frame cap */
-    int error;
+    vka_object_t serial = {0};
+    int error = vka_alloc_untyped_at(&env->vka, seL4_PageBits, DEFAULT_SERIAL_PADDR,
+                                     &serial);
+    ZF_LOGF_IF(error, "Failed to allocate untyped for serial at %x\n",
+               DEFAULT_SERIAL_PADDR);
 
-    error = vka_cspace_alloc_path(&env->vka, &env->serial_frame_path);
-    if (error != 0) {
-        ZF_LOGE("Failed to alloc slot for serial Frame cap.");
-        return error;
-    }
+    vka_cspace_make_path(&env->vka, serial.cptr, &env->serial_frame_path);
     env->serial_frame_paddr = DEFAULT_SERIAL_PADDR;
-    error = simple_get_frame_cap(&env->simple, (void *) env->serial_frame_paddr,
-                                 PAGE_BITS_4K, &env->serial_frame_path);
-    if (error != 0) {
-        ZF_LOGE("Failed to get Frame cap for serial device.");
-        return error;
-    }
     return 0;
 }
 

apps/sel4test-driver/src/main.c
@@ -23,6 +23,7 @@
 #include <platsupport/timer.h>
 
 #include <sel4debug/register_dump.h>
+#include <sel4platsupport/device.h>
 #include <sel4platsupport/platsupport.h>
 #include <sel4platsupport/plat/timer.h>
 #include <sel4utils/vspace.h>
@@ -224,6 +225,11 @@ copy_timer_caps(test_init_data_t *init, env_t env, sel4utils_process_t *test_pro
     init->timer_irq = copy_cap_to_process(test_process, env->irq_path.capPtr);
     assert(init->timer_irq != 0);
 
+    /* untyped cap for timer device untyped */
+    init->timer_paddr = env->timer_paddr;
+    init->timer_untyped = copy_cap_to_process(test_process, env->timer_untyped.cptr);
+    assert(init->timer_untyped != 0);
+
     arch_copy_timer_caps(init, env, test_process);
 }
 
@@ -330,19 +336,15 @@ run_test(struct testcase *test)
 static void
 init_timer_caps(env_t env)
 {
-    /* get the irq control cap */
+    /* allocate a cslot for the irq control cap */
     seL4_CPtr cap;
     int error = vka_cspace_alloc(&env->vka, &cap);
-    if (error != 0) {
-        ZF_LOGF("Failed to allocate cslot, error %d", error);
-    }
-
+    ZF_LOGF_IF(error, "Failed to allocate cslot, error %d", error);
     vka_cspace_make_path(&env->vka, cap, &env->irq_path);
-    error = simple_get_IRQ_control(&env->simple, DEFAULT_TIMER_INTERRUPT, env->irq_path);
 
-    if (error != 0) {
-        ZF_LOGF("Failed to get IRQ control, error %d", error);
-    }
+    env->timer_paddr = sel4platsupport_get_default_timer_paddr(&env->vka, &env->vspace);
+    error = vka_alloc_untyped_at(&env->vka, seL4_PageBits, env->timer_paddr, &env->timer_untyped);
+    ZF_LOGF_IF(error, "Failed to get untyped at paddr %x for timer\n", env->timer_paddr);
 
     arch_init_timer_caps(env);
 }

apps/sel4test-tests/src/arch/arm/arch.c
@@ -7,42 +7,33 @@
  *
  * @TAG(NICTA_BSD)
  */
-#include "../../test.h"
 
-#include <sel4platsupport/timer.h>
-#include <sel4platsupport/plat/timer.h>
+#include "../../init.h"
 #include <sel4platsupport/plat/serial.h>
-#include <platsupport/timer.h>
 
-static seL4_Error
-get_frame_cap(void *data, void *paddr, int size_bits, cspacepath_t *path)
+void
+arch_init_allocator(allocman_t *alloc, vka_t *vka, test_init_data_t *data)
 {
-    seL4_CPtr src_cap = 0;
 
-    test_init_data_t *init = (test_init_data_t *) data;
+    /* add serial frame */
+    size_t size_bits = seL4_PageBits;
+    uintptr_t paddr = DEFAULT_SERIAL_PADDR;
+    cspacepath_t path;
+    vka_cspace_make_path(vka, data->serial_frame, &path);
+    int error = allocman_utspace_add_uts(alloc, 1, &path,
+                                         &size_bits, &paddr,
+                                         ALLOCMAN_UT_DEV);
+    ZF_LOGF_IF(error, "Failed to add serial ut to allocator");
+}
 
-    assert(size_bits == seL4_PageBits);
-    switch ((uintptr_t)paddr) {
-    case DEFAULT_TIMER_PADDR:
-        src_cap = init->timer_frame;
-        break;
-    case DEFAULT_SERIAL_PADDR:
-        src_cap = init->serial_frame;
-        break;
-    default:
-        ZF_LOGF("Unsupported paddr %p requested. No Frame cap available.", paddr);
-    }
-
-    int error = seL4_CNode_Copy(path->root, path->capPtr, path->capDepth, init->root_cnode,
-                                src_cap, seL4_WordBits, seL4_AllRights);
-    assert(error == seL4_NoError);
-
-    return error;
+seL4_timer_t *
+arch_init_timer(env_t env, test_init_data_t *data)
+{
+   return sel4platsupport_get_default_timer(&env->vka, &env->vspace, &env->simple,
+                                            env->timer_notification.cptr);
 }
 
 void
-arch_init_simple(simple_t *simple)
-{
-    simple->frame_cap = get_frame_cap;
+arch_init_simple(simple_t *simple) {
+    /* nothing to do */
 }
-

apps/sel4test-tests/src/arch/x86/arch.c
@@ -7,36 +7,51 @@
  *
  * @TAG(NICTA_BSD)
  */
-#include "../../test.h"
-
-#include <sel4platsupport/timer.h>
-#include <sel4platsupport/plat/timer.h>
-#include <platsupport/timer.h>
+#include "../../init.h"
 
 #include <platsupport/plat/serial.h>
-#include <platsupport/plat/pit.h>
+#include <sel4platsupport/plat/hpet.h>
 
 static seL4_CPtr
 get_IOPort_cap(void *data, uint16_t start_port, uint16_t end_port)
 {
     test_init_data_t *init = (test_init_data_t *) data;
 
-    assert((start_port >= PIT_IO_PORT_MIN && end_port <= PIT_IO_PORT_MAX)
-            || (start_port >= SERIAL_CONSOLE_COM1_PORT && start_port <= SERIAL_CONSOLE_COM1_PORT_END));
+    assert(start_port >= SERIAL_CONSOLE_COM1_PORT &&
+           start_port <= SERIAL_CONSOLE_COM1_PORT_END);
 
-    if (start_port >= PIT_IO_PORT_MIN && end_port <= PIT_IO_PORT_MAX) {
-        return init->io_port;
-    }
+    return init->serial_io_port1;
+}
 
-    if (start_port >= SERIAL_CONSOLE_COM1_PORT && start_port <= SERIAL_CONSOLE_COM1_PORT_END) {
-        return init->serial_io_port1;
-    }
-    /* Return invalid cap */
-    return 0;
+static seL4_Error
+get_msi(void *data, seL4_CNode root, seL4_Word index, uint8_t depth,
+        UNUSED seL4_Word pci_bus, UNUSED seL4_Word pci_dev, UNUSED seL4_Word pci_func,
+        UNUSED seL4_Word handle, seL4_Word vector)
+{
+    assert(vector == IRQ_OFFSET);
+    test_init_data_t *init = (test_init_data_t *) data;
+    int error = seL4_CNode_Move(root, index, depth, init->root_cnode,
+            init->timer_irq, seL4_WordBits);
+    assert(error == seL4_NoError);
+    return seL4_NoError;
 }
 
 void
 arch_init_simple(simple_t *simple) {
     simple->arch_simple.IOPort_cap = get_IOPort_cap;
+    simple->arch_simple.msi = get_msi;
 }
 
+seL4_timer_t *
+arch_init_timer(env_t env, test_init_data_t *data)
+{
+   return sel4platsupport_get_hpet_paddr(&env->vspace, &env->simple, &env->vka,
+                                         data->timer_paddr, env->timer_notification.cptr,
+                                         DEFAULT_TIMER_INTERRUPT);
+}
+
+void
+arch_init_allocator(allocman_t *alloc, vka_t *vka, test_init_data_t *data)
+{
+    /* nothing to do */
+}

apps/sel4test-tests/src/init.h
@@ -1,0 +1,20 @@
+/*
+ *  Copyright 2016, Data61
+ *  Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ *  ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(D61_BSD)
+ */
+#pragma once
+
+#include <allocman/allocman.h>
+
+#include "helpers.h"
+
+void arch_init_allocator(allocman_t *alloc, vka_t *vka, test_init_data_t *data);
+seL4_timer_t *arch_init_timer(env_t env, test_init_data_t *data);
+void arch_init_simple(simple_t *simple);

apps/sel4test-tests/src/main.c
@@ -34,6 +34,7 @@
 
 #include "helpers.h"
 #include "test.h"
+#include "init.h"
 
 /* dummy global for libsel4muslcsys */
 char _cpio_archive[1];
@@ -122,21 +123,32 @@ init_allocator(env_t env, test_init_data_t *init_data)
     /* fill the allocator with untypeds */
     seL4_CPtr slot;
     unsigned int size_bits_index;
+    size_t size_bits;
+    cspacepath_t path;
     for (slot = init_data->untypeds.start, size_bits_index = 0;
             slot <= init_data->untypeds.end;
             slot++, size_bits_index++) {
 
-        cspacepath_t path;
         vka_cspace_make_path(&env->vka, slot, &path);
         /* allocman doesn't require the paddr unless we need to ask for phys addresses,
          * which we don't. */
-        uintptr_t fake_paddr = 0;
-        size_t size_bits = init_data->untyped_size_bits_list[size_bits_index];
-        error = allocman_utspace_add_uts(allocator, 1, &path, &size_bits, &fake_paddr, ALLOCMAN_UT_KERNEL);
+        size_bits = init_data->untyped_size_bits_list[size_bits_index];
+        error = allocman_utspace_add_uts(allocator, 1, &path, &size_bits, NULL,
+                                         ALLOCMAN_UT_KERNEL);
         if (error) {
             ZF_LOGF("Failed to add untyped objects to allocator");
         }
     }
+
+    /* add timeout timer to the allocator */
+    size_bits = seL4_PageBits;
+    vka_cspace_make_path(&env->vka, init_data->timer_untyped, &path);
+    error = allocman_utspace_add_uts(allocator, 1, &path, &size_bits,
+                                     &init_data->timer_paddr, ALLOCMAN_UT_DEV);
+    ZF_LOGF_IF(error, "Failed to add timer ut to allocator");
+
+    /* add any arch specific objects to the allocator */
+    arch_init_allocator(allocator, &env->vka, init_data);
 
     /* create a vspace */
     void *existing_frames[init_data->stack_pages + 2];
@@ -168,10 +180,10 @@ get_irq(void *data, int irq, seL4_CNode root, seL4_Word index, uint8_t depth)
     test_init_data_t *init = (test_init_data_t *) data;
     assert(irq == DEFAULT_TIMER_INTERRUPT);
 
-    int error = seL4_CNode_Copy(root, index, depth, init->root_cnode,
-                                init->timer_irq, seL4_WordBits, seL4_AllRights);
+    int error = seL4_CNode_Move(root, index, depth, init->root_cnode,
+                                init->timer_irq, seL4_WordBits);
     if (error != 0) {
-        ZF_LOGF("Failed to copy irq cap\n");
+        ZF_LOGF("Failed to move irq cap\n");
     }
 
     return error;
@@ -185,17 +197,14 @@ void init_timer(env_t env, test_init_data_t *init_data)
     env->simple.data = (void *) init_data;
     env->simple.arch_simple.data = (void *) init_data;
 
-    UNUSED int error;
-
     arch_init_simple(&env->simple);
 
-    error = vka_alloc_notification(&env->vka, &env->timer_notification);
+    int error = vka_alloc_notification(&env->vka, &env->timer_notification);
     if (error != 0) {
         ZF_LOGF("Failed to allocate notification object");
     }
 
-    env->timer = sel4platsupport_get_default_timer(&env->vka, &env->vspace,
-                                                   &env->simple, env->timer_notification.cptr);
+    env->timer = arch_init_timer(env, init_data);
     if (env->timer == NULL) {
         ZF_LOGF("Failed to initialise default timer");
     }
@@ -233,7 +242,7 @@ main(int argc, char **argv)
     env.cspace_size_bits = init_data->cspace_size_bits;
     env.tcb = init_data->tcb;
     env.domain = init_data->domain;
-    env.timer_frame = init_data->timer_frame;
+    env.timer_untyped = init_data->timer_untyped;
     env.asid_pool = init_data->asid_pool;
     env.asid_ctrl = init_data->asid_ctrl;
 #ifdef CONFIG_IOMMU

apps/sel4test-driver/src/arch/x86/arch.c
@@ -15,10 +15,8 @@
 void
 arch_init_timer_caps(env_t env)
 {
-    env->io_port_cap = simple_get_IOPort_cap(&env->simple, PIT_IO_PORT_MIN, PIT_IO_PORT_MAX);
-    if (env->io_port_cap == 0) {
-        ZF_LOGF("Failed to get IO port cap for range %x to %x.", PIT_IO_PORT_MIN, PIT_IO_PORT_MAX);
-    }
+    int error = arch_simple_get_msi(&env->simple.arch_simple, env->irq_path, 0, 0, 0, 0, DEFAULT_TIMER_INTERRUPT);
+    ZF_LOGF_IF(error, "Failed to copy irq cap");
 }
 
 int
@@ -81,8 +79,6 @@ arch_init_serial_caps(env_t env)
 void
 arch_copy_timer_caps(test_init_data_t *init, env_t env, sel4utils_process_t *test_process)
 {
-    /* io port cap (since the default timer on ia32 is the PIT) */
-    init->io_port = copy_cap_to_process(test_process, env->io_port_cap);
 }
 
 void

apps/sel4test-driver/include/test_init_data.h
@@ -54,7 +54,10 @@ typedef struct {
     /* cap to the sel4platsupport default timer irq handler */
     seL4_CPtr timer_irq;
     /* cap to the sel4platsupport default timer physical frame */
-    seL4_CPtr timer_frame;
+    seL4_CPtr timer_untyped;
+    /* physical address of the timeout timer */
+    uintptr_t timer_paddr;
+
     /* cap to the sel4platsupport default timer io port */
     seL4_CPtr io_port;
 

commit 0fe04258adbca8fec677a87663d198df1b993baa

sel4test: fix arm serial broken in 35944e1e613f380

apps/sel4test-driver/src/arch/arm/arch.c
@@ -31,9 +31,9 @@ arch_copy_timer_caps(test_init_data_t *init, env_t env, sel4utils_process_t *tes
 int
 arch_init_serial_caps(env_t env)
 {
-    /* Get the serial frame cap */
+    /* Get the serial untyped cap - this is what we give the allocator */
     vka_object_t serial = {0};
-    int error = vka_alloc_untyped_at(&env->vka, seL4_PageBits, DEFAULT_SERIAL_PADDR,
+    int error = vka_alloc_frame_at(&env->vka, seL4_PageBits, DEFAULT_SERIAL_PADDR,
                                      &serial);
     ZF_LOGF_IF(error, "Failed to allocate untyped for serial at %x\n",
                DEFAULT_SERIAL_PADDR);

apps/sel4test-driver/src/main.c
@@ -349,16 +349,6 @@ init_timer_caps(env_t env)
     arch_init_timer_caps(env);
 }
 
-static void
-init_serial_caps(env_t env)
-{
-    int error;
-
-    error = arch_init_serial_caps(env);
-    ZF_LOGF_IF(error != 0,
-               "Err %d: Failed to initialize platform serial caps.", error);
-}
-
 void *main_continued(void *arg UNUSED)
 {
 
@@ -414,13 +404,17 @@ void *main_continued(void *arg UNUSED)
     return NULL;
 }
 
-static seL4_Error serial_frame_cap_wrapper(void *data, void *paddr, int size_bits, cspacepath_t *path)
+static int serial_utspace_alloc_at_fn(void *data, const cspacepath_t *dest, seL4_Word type, seL4_Word size_bits,
+        uintptr_t paddr, seL4_Word *cookie)
 {
     if (env.serial_frame_paddr == (uintptr_t)paddr) {
-        return vka_cnode_copy(path, &env.serial_frame_path, seL4_AllRights);
+        return vka_cnode_copy(dest, &env.serial_frame_path, seL4_AllRights);
     }
-    return env.simple.frame_cap(data, paddr, size_bits, path);
+
+    assert(env.vka.utspace_alloc_at);
+    return env.vka.utspace_alloc_at(data, dest, type, size_bits, paddr, cookie);
 }
+
 
 int main(void)
 {
@@ -438,20 +432,24 @@ int main(void)
     /* initialise the test environment - allocator, cspace manager, vspace manager, timer */
     init_env(&env);
 
-    /* Initialize the serial so that we can start using it ourselves */
-    init_serial_caps(&env);
+    /* initialise the serial cap - sel4test-driver and sel4test-tests use this cap, so we need
+     * to allocate it and make copies of it to hand out */
+    int error = arch_init_serial_caps(&env);
+    ZF_LOGF_IF(error != 0,
+               "Err %d: Failed to initialize platform serial caps.", error);
 
-    /* Construct a simple wrapper for returning the serial frame. We need to create this
-     * wrapper as the actual simple implementation will only allocate/return any given
+    /* Construct a vka wrapper for returning the serial frame. We need to create this
+     * wrapper as the actual vka implementation will only allocate/return any given
      * device frame once. As we already allocated it in init_serial_caps when we the
      * platsupport_serial_setup_simple attempts to allocate it will fail. This wrapper
      * just returns a copy of the one we already allocated, whilst passing all other
-     * requests on to the actual simple */
-    simple_t serial_simple = env.simple;
-    serial_simple.frame_cap = serial_frame_cap_wrapper;
+     * requests on to the actual vka */
+    vka_t serial_vka = env.vka;
+    serial_vka.utspace_alloc_at = serial_utspace_alloc_at_fn;
+
 
     /* enable serial driver */
-    platsupport_serial_setup_simple(NULL, &serial_simple, &env.vka);
+    platsupport_serial_setup_simple(&env.vspace, &env.simple, &serial_vka);
 
     simple_print(&env.simple);
     /* switch to a bigger, safer stack with a guard page
@@ -459,7 +457,7 @@ int main(void)
     printf("Switching to a safer, bigger stack... ");
     fflush(stdout);
     void *res;
-    int error = sel4utils_run_on_stack(&env.vspace, main_continued, NULL, &res);
+    error = sel4utils_run_on_stack(&env.vspace, main_continued, NULL, &res);
     test_assert_fatal(error == 0);
     test_assert_fatal(res == 0);
 

apps/sel4test-tests/src/arch/arm/arch.c
@@ -10,20 +10,27 @@
 
 #include "../../init.h"
 #include <sel4platsupport/plat/serial.h>
+#include <vka/capops.h>
+
+static cspacepath_t serial_path = {0};
+static vka_t old_vka;
+
+static int
+serial_utspace_alloc_at_fn(void *data, const cspacepath_t *dest, seL4_Word type, seL4_Word size_bits,
+        uintptr_t paddr, seL4_Word *cookie)
+{
+    if (paddr == DEFAULT_SERIAL_PADDR) {
+        return vka_cnode_copy(dest, &serial_path, seL4_AllRights);
+    }
+    return old_vka.utspace_alloc_at(data, dest, type, size_bits, paddr, cookie);
+}
 
 void
-arch_init_allocator(allocman_t *alloc, vka_t *vka, test_init_data_t *data)
+arch_init_allocator(env_t env, test_init_data_t *data)
 {
-
-    /* add serial frame */
-    size_t size_bits = seL4_PageBits;
-    uintptr_t paddr = DEFAULT_SERIAL_PADDR;
-    cspacepath_t path;
-    vka_cspace_make_path(vka, data->serial_frame, &path);
-    int error = allocman_utspace_add_uts(alloc, 1, &path,
-                                         &size_bits, &paddr,
-                                         ALLOCMAN_UT_DEV);
-    ZF_LOGF_IF(error, "Failed to add serial ut to allocator");
+    vka_cspace_make_path(&env->vka, data->serial_frame, &serial_path);
+    old_vka = env->vka;
+    env->vka.utspace_alloc_at = serial_utspace_alloc_at_fn;
 }
 
 seL4_timer_t *

apps/sel4test-tests/src/arch/x86/arch.c
@@ -51,7 +51,7 @@ arch_init_timer(env_t env, test_init_data_t *data)
 }
 
 void
-arch_init_allocator(allocman_t *alloc, vka_t *vka, test_init_data_t *data)
+arch_init_allocator(env_t env, test_init_data_t *data)
 {
     /* nothing to do */
 }

apps/sel4test-tests/src/init.h
@@ -15,6 +15,6 @@
 
 #include "helpers.h"
 
-void arch_init_allocator(allocman_t *alloc, vka_t *vka, test_init_data_t *data);
+void arch_init_allocator(env_t env, test_init_data_t *data);
 seL4_timer_t *arch_init_timer(env_t env, test_init_data_t *data);
 void arch_init_simple(simple_t *simple);

apps/sel4test-tests/src/main.c
@@ -148,7 +148,7 @@ init_allocator(env_t env, test_init_data_t *init_data)
     ZF_LOGF_IF(error, "Failed to add timer ut to allocator");
 
     /* add any arch specific objects to the allocator */
-    arch_init_allocator(allocator, &env->vka, init_data);
+    arch_init_allocator(env, init_data);
 
     /* create a vspace */
     void *existing_frames[init_data->stack_pages + 2];