23271e289dc4a60c7a3290421c3f9455a7994996
Adrian Danis <Adrian.Danis@data61.csiro.au>
2017-01-11 17:31:05 +1100

    x86: Support IOAPIC delivery for the HPET
    Instead of assuming MSI delivery for the HPET this change allows us
    to at run time query the capabilities of the HPET and use FSB
    or IOAPIC delivery as available.
    
    The motivation for this is many simulators (such as qemu) do not
    emulate FSB delivery for the HPET preventing tests requiring a
    timer from being run on them

--- apps/sel4test-tests/src/arch/x86/arch.c
+++ apps/sel4test-tests/src/arch/x86/arch.c
@@ -11,6 +11,8 @@
 
 #include <platsupport/plat/serial.h>
 #include <sel4platsupport/plat/hpet.h>
+#include <sel4platsupport/device.h>
+#include <platsupport/plat/hpet.h>
 
 static seL4_CPtr
 get_IOPort_cap(void *data, uint16_t start_port, uint16_t end_port)
@@ -28,26 +30,57 @@ get_msi(void *data, seL4_CNode root, seL4_Word index, uint8_t depth,
         UNUSED seL4_Word pci_bus, UNUSED seL4_Word pci_dev, UNUSED seL4_Word pci_func,
         UNUSED seL4_Word handle, seL4_Word vector)
 {
-    assert(vector == IRQ_OFFSET);
     test_init_data_t *init = (test_init_data_t *) data;
     int error = seL4_CNode_Move(root, index, depth, init->root_cnode,
             init->timer_irq_cap, seL4_WordBits);
     assert(error == seL4_NoError);
-    return seL4_NoError;
+    return error;
+}
+
+static seL4_Error
+get_ioapic(void *data, seL4_CNode root, seL4_Word index, uint8_t depth, seL4_Word ioapic,
+           seL4_Word pin, seL4_Word level, seL4_Word polarity, seL4_Word vector)
+{
+    test_init_data_t *init = (test_init_data_t *) data;
+    int error = seL4_CNode_Move(root, index, depth, init->root_cnode,
+            init->timer_irq_cap, seL4_WordBits);
+    assert(error == seL4_NoError);
+    return error;
 }
 
 void
 arch_init_simple(simple_t *simple) {
     simple->arch_simple.IOPort_cap = get_IOPort_cap;
     simple->arch_simple.msi = get_msi;
+    simple->arch_simple.ioapic = get_ioapic;
 }
 
 seL4_timer_t *
 arch_init_timer(env_t env, test_init_data_t *data)
 {
+    /* Map the HPET so we can query its proprties */
+    vka_object_t frame;
+    void *vaddr;
+    vaddr = sel4platsupport_map_frame_at(&env->vka, &env->vspace, data->timer_paddr, seL4_PageBits, &frame);
+    int irq;
+    int vector;
+    ZF_LOGF_IF(vaddr == NULL, "Failed to map HPET paddr");
+    if (!hpet_supports_fsb_delivery(vaddr)) {
+        if (!config_set(CONFIG_IRQ_IOAPIC)) {
+            ZF_LOGF("HPET does not support FSB delivery and we are not using the IOAPIC");
+        }
+        uint32_t irq_mask = hpet_ioapic_irq_delivery_mask(vaddr);
+        /* grab the first irq */
+        irq = FFS(irq_mask) - 1;
+    } else {
+        irq = -1;
+    }
+    vector = DEFAULT_TIMER_INTERRUPT;
+    vspace_unmap_pages(&env->vspace, vaddr, 1, seL4_PageBits, VSPACE_PRESERVE);
+    vka_free_object(&env->vka, &frame);
     return sel4platsupport_get_hpet_paddr(&env->vspace, &env->simple, &env->vka,
                                          data->timer_paddr, env->timer_notification.cptr,
-                                         DEFAULT_TIMER_INTERRUPT);
+                                         irq, vector);
 }
 
 void
