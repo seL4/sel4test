947f9042e126ffbe383bf1b5a804cd71e8ad66d6
Claudia Tu <claudia.tu@unsw.edu.au>
2017-09-01 12:40:28 +1000

    SELFOUR-1073 find test_type section when extracting test names

--- apps/sel4test-driver/scripts/extract-test-names.sh
+++ apps/sel4test-driver/scripts/extract-test-names.sh
@@ -19,4 +19,4 @@ if [ $# -ne 2 ]; then
 fi
 
 echo "#include <sel4test/test.h>"
-echo ""; $1 -t -j _test_case $2 | grep -E " [lg][ ]+O _test_case.*TEST_" | tr -s ' ' | cut -d ' ' -f 5 | sort | while read line; do echo "__attribute__((used)) __attribute__((section(\"_test_case\"))) testcase_t ${line} = { .name = \"${line}\"};"; done; echo ""
+echo ""; $1 -t -j _test_case $2 | grep -E " [lg][ ]+O _test_case.*TEST_" | tr -s ' ' | cut -d ' ' -f 5 | sort | while read line; do echo "__attribute__((used)) __attribute__((section(\"_test_case\"))) testcase_t ${line} = { .name = \"${line}\", .test_type = BASIC};"; done; echo ""
