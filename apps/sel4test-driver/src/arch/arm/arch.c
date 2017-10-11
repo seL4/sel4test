seL4/sel4test

commit b00349661465ea11de780640c75766f330afbdaf

sel4test-driver: Make different environment types clearer
Changes the 'env' in sel4test-driver to be called driver_env so that it is not confused
with the environment 'env' in the basic tests. To handle this type change we also
explicitly declare implementations that could take an arbitrary environment to take a
'uintptr_t' type that is cast to the known (and hopefully correct) environment type.

apps/sel4test-driver/src/arch/arm/arch.c
@@ -18,7 +18,7 @@
 #include <sel4utils/process.h>
 
 void
-arch_copy_serial_caps(test_init_data_t *init, env_t env, sel4utils_process_t *test_process)
+arch_copy_serial_caps(test_init_data_t *init, driver_env_t env, sel4utils_process_t *test_process)
 {
     init->serial_paddr = env->serial_objects.arch_serial_objects.serial_frame_paddr;
     init->serial_frame_cap = sel4utils_copy_cap_to_process(test_process, &env->vka, env->serial_objects.arch_serial_objects.serial_frame_obj.cptr);
@@ -26,7 +26,7 @@ arch_copy_serial_caps(test_init_data_t *init, env_t env, sel4utils_process_t *te
                "Failed to copy PS default serial Frame cap to sel4test-test process");
 }
 
-env_t env;
+extern driver_env_t env;
 int serial_utspace_alloc_at_fn(void *data, const cspacepath_t *dest, seL4_Word type, seL4_Word size_bits,
                                uintptr_t paddr, seL4_Word *cookie)
 {
@@ -43,7 +43,7 @@ int serial_utspace_alloc_at_fn(void *data, const cspacepath_t *dest, seL4_Word t
     return env->vka.utspace_alloc_at(data, dest, type, size_bits, paddr, cookie);
 }
 
-vka_utspace_alloc_at_fn arch_get_serial_utspace_alloc_at(env_t _env)
+vka_utspace_alloc_at_fn arch_get_serial_utspace_alloc_at(driver_env_t _env)
 {
     static bool call_once = false;
     if (call_once) {
@@ -56,7 +56,7 @@ vka_utspace_alloc_at_fn arch_get_serial_utspace_alloc_at(env_t _env)
 
 #ifdef CONFIG_ARM_SMMU
 seL4_SlotRegion
-arch_copy_iospace_caps_to_process(sel4utils_process_t *process, env_t env)
+arch_copy_iospace_caps_to_process(sel4utils_process_t *process, driver_env_t env)
 {
     seL4_SlotRegion ret = {0, 0};
     int num_iospace_caps = 0;

apps/sel4test-driver/src/arch/x86/arch.c
@@ -19,7 +19,7 @@
 #include <vka/capops.h>
 
 void
-arch_copy_serial_caps(test_init_data_t *init, env_t env, sel4utils_process_t *test_process)
+arch_copy_serial_caps(test_init_data_t *init, driver_env_t env, sel4utils_process_t *test_process)
 {
     init->serial_irq_cap = sel4utils_copy_cap_to_process(test_process, &env->vka, env->serial_objects.serial_irq_path.capPtr);
     init->serial_io_port_cap = sel4utils_copy_cap_to_process(test_process, &env->vka, env->serial_objects.arch_serial_objects.serial_io_port_cap);
@@ -32,7 +32,7 @@ int serial_utspace_alloc_at_fn(void *data, const cspacepath_t *dest, seL4_Word t
     return -1;
 }
 
-vka_utspace_alloc_at_fn arch_get_serial_utspace_alloc_at(env_t _env)
+vka_utspace_alloc_at_fn arch_get_serial_utspace_alloc_at(driver_env_t _env)
 {
     static bool call_once = false;
     if (call_once) {

apps/sel4test-driver/src/main.c
@@ -62,7 +62,7 @@ static char allocator_mem_pool[ALLOCATOR_STATIC_POOL_SIZE];
 static sel4utils_alloc_data_t data;
 
 /* environment encapsulating allocation interfaces etc */
-static struct env env;
+struct driver_env env;
 /* list of untypeds to give out to test processes */
 static vka_object_t untypeds[CONFIG_MAX_NUM_BOOTINFO_UNTYPED_CAPS];
 /* list of sizes (in bits) corresponding to untyped */
@@ -70,7 +70,7 @@ static uint8_t untyped_size_bits_list[CONFIG_MAX_NUM_BOOTINFO_UNTYPED_CAPS];
 
 /* initialise our runtime environment */
 static void
-init_env(env_t env)
+init_env(driver_env_t env)
 {
     allocman_t *allocman;
     reservation_t virtual_reservation;
@@ -264,7 +264,7 @@ static int collate_tests(testcase_t *tests_in, int n, testcase_t *tests_out[], i
     return out_index;
 }
 
-void sel4test_run_tests(struct env* e)
+void sel4test_run_tests(struct driver_env* e)
 {
     /* Iterate through test types. */
     int max_test_types = (int) (__stop__test_type - __start__test_type);
@@ -327,20 +327,20 @@ void sel4test_run_tests(struct env* e)
     for (int tt = 0; tt < num_test_types; tt++) {
         /* set up */
         if (test_types[tt]->set_up_test_type != NULL) {
-            test_types[tt]->set_up_test_type(e);
+            test_types[tt]->set_up_test_type((uintptr_t)e);
         }
 
         for (int i = 0; i < num_tests; i++) {
             if (tests[i]->test_type == test_types[tt]->id) {
                 sel4test_start_test(tests[i]->name, tests_done);
                 if (test_types[tt]->set_up != NULL) {
-                    test_types[tt]->set_up(e);
+                    test_types[tt]->set_up((uintptr_t)e);
                 }
 
-                test_result_t result = test_types[tt]->run_test(tests[i], e);
+                test_result_t result = test_types[tt]->run_test(tests[i], (uintptr_t)e);
 
                 if (test_types[tt]->tear_down != NULL) {
-                    test_types[tt]->tear_down(e);
+                    test_types[tt]->tear_down((uintptr_t)e);
                 }
                 sel4test_end_test(result);
 
@@ -357,7 +357,7 @@ void sel4test_run_tests(struct env* e)
 
         /* tear down */
         if (test_types[tt]->tear_down_test_type != NULL) {
-            test_types[tt]->tear_down_test_type(e);
+            test_types[tt]->tear_down_test_type((uintptr_t)e);
         }
     }
 

apps/sel4test-driver/src/plat/allwinnerA20/plat.c
@@ -12,7 +12,7 @@
 #include "../../test.h"
 
 void
-plat_init(env_t env)
+plat_init(driver_env_t env)
 {
     /* clock timer not implemented for this platform */
 }

apps/sel4test-driver/src/plat/am335x/plat.c
@@ -17,7 +17,7 @@
 #include <sel4platsupport/device.h>
 
 void
-plat_init(env_t env)
+plat_init(driver_env_t env)
 {
     /* clock timer not implemented for this platform */
 }

apps/sel4test-driver/src/plat/apq8064/plat.c
@@ -12,7 +12,7 @@
 #include "../../test.h"
 
 void
-plat_init(env_t env)
+plat_init(driver_env_t env)
 {
     /* clock timer not implemented for this platform */
 }

apps/sel4test-driver/src/plat/bcm2837/plat.c
@@ -12,7 +12,7 @@
 #include "../../test.h"
 
 void
-plat_init(env_t env)
+plat_init(driver_env_t env)
 {
     /* clock timer not implemented for this platform */
 }

apps/sel4test-driver/src/plat/exynos4/plat.c
@@ -12,6 +12,6 @@
 #include "../../test.h"
 
 void
-plat_init(env_t env)
+plat_init(driver_env_t env)
 {
 }

commit 543bfa06b83d36f197ed70da365eb6d485e8057f

sel4test-driver: Fix and clarify global environment wrapper
This was broken by b00349661465 and this commit corrects the variable here to be a static
local as it was intended, and fixes names and comments to not collide with the other global
`env`

apps/sel4test-driver/src/arch/arm/arch.c
@@ -26,21 +26,25 @@ arch_copy_serial_caps(test_init_data_t *init, driver_env_t env, sel4utils_proces
                "Failed to copy PS default serial Frame cap to sel4test-test process");
 }
 
-extern driver_env_t env;
+/* global used for storing the environment as we are going to be overriding one function
+ * of a vka interface, but we do not bother to create an entire interface wrapper with
+ * a new 'data' cookie */
+static driver_env_t alloc_at_env;
 int serial_utspace_alloc_at_fn(void *data, const cspacepath_t *dest, seL4_Word type, seL4_Word size_bits,
                                uintptr_t paddr, seL4_Word *cookie)
 {
-    if (paddr == env->serial_objects.arch_serial_objects.serial_frame_paddr) {
+    ZF_LOGF_IF(!alloc_at_env, "%s called before arch_get_serial_utspace_alloc_at", __FUNCTION__);
+    if (paddr == alloc_at_env->serial_objects.arch_serial_objects.serial_frame_paddr) {
         cspacepath_t tmp_frame_path;
 
-        vka_cspace_make_path(&env->vka,
-                             env->serial_objects.arch_serial_objects.serial_frame_obj.cptr,
+        vka_cspace_make_path(&alloc_at_env->vka,
+                             alloc_at_env->serial_objects.arch_serial_objects.serial_frame_obj.cptr,
                              &tmp_frame_path);
         return vka_cnode_copy(dest, &tmp_frame_path, seL4_AllRights);
     }
 
-    assert(env->vka.utspace_alloc_at);
-    return env->vka.utspace_alloc_at(data, dest, type, size_bits, paddr, cookie);
+    assert(alloc_at_env->vka.utspace_alloc_at);
+    return alloc_at_env->vka.utspace_alloc_at(data, dest, type, size_bits, paddr, cookie);
 }
 
 vka_utspace_alloc_at_fn arch_get_serial_utspace_alloc_at(driver_env_t _env)
@@ -50,7 +54,7 @@ vka_utspace_alloc_at_fn arch_get_serial_utspace_alloc_at(driver_env_t _env)
         ZF_LOGF("This function can only be called once.");
     }
     call_once = true;
-    env = _env;
+    alloc_at_env = _env;
     return serial_utspace_alloc_at_fn;
 }
 