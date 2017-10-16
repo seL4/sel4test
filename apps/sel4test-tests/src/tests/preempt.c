1835f248c5710e46142a40b023411ce7d500d811
Hesham Almatary <hesham.almatary@unsw.edu.au>
2017-10-16 16:17:15 +1100

    Fix PREEMPT_REVOKE: Set timeout value to half of the revoke operation time

--- apps/sel4test-tests/src/tests/preempt.c
+++ apps/sel4test-tests/src/tests/preempt.c
@@ -42,10 +42,45 @@ preempt_count_func(env_t env)
 }
 
 static int
+create_cnode_table(env_t env, int num_cnode_bits, seL4_CPtr ep) {
+    /* Create as many cnodes as possible. We will copy the cap into all
+     * those cnodes. */
+#define CNODE_SIZE_BITS 12
+    int num_caps = 0;
+    int error = 0;
+
+    ZF_LOGD("    Creating %d caps .", BIT((num_cnode_bits + CNODE_SIZE_BITS)));
+    for (int i = 0; i < (1 << num_cnode_bits); i++) {
+        seL4_CPtr ctable = vka_alloc_cnode_object_leaky(&env->vka, CNODE_SIZE_BITS);
+
+        for (int j = 0; j < BIT(CNODE_SIZE_BITS); j++) {
+            error = seL4_CNode_Copy(
+                        ctable, j, CNODE_SIZE_BITS,
+                        env->cspace_root, ep, seL4_WordBits,
+                        seL4_AllRights);
+
+            test_check(!error);
+            num_caps++;
+        }
+        ZF_LOGD(".");
+    }
+
+    if (num_caps != BIT(num_cnode_bits + CNODE_SIZE_BITS)) {
+        ZF_LOGD("Created %d caps. Couldn't create the required number of caps %d",
+           num_caps,
+           BIT(num_cnode_bits + CNODE_SIZE_BITS));
+        return -1;
+    }
+
+    return error;
+}
+
+static int
 test_preempt_revoke_actual(env_t env, int num_cnode_bits)
 {
     helper_thread_t revoke_thread, preempt_thread;
     int error;
+    uint64_t timeout_val = 0;
 
     /* Create an endpoint cap that will be derived many times. */
     seL4_CPtr ep = vka_alloc_endpoint_leaky(&env->vka);
@@ -56,31 +91,38 @@ test_preempt_revoke_actual(env_t env, int num_cnode_bits)
     set_helper_priority(env, &preempt_thread, 101);
     set_helper_priority(env, &revoke_thread, 100);
 
-    /* Now create as many cnodes as possible. We will copy the cap into all
-     * those cnodes. */
-    int num_caps = 0;
+    /* First create a ctable and fill it with copied ep caps
+     * and time the revoke operation
+     */
+    error = create_cnode_table(env, num_cnode_bits, ep);
+    test_assert(!error);
 
-#define CNODE_SIZE_BITS 12
+    uint64_t start, end, diff;
+    /* meaure revoke_func time.
+     * Note that we don't take caching and other effects (e.g. function calls vs.
+     * jmp costs into consideration, however, this should get us a better
+     * timeout value per target compared to setting a fixed timeout value that may
+     * fail on different targets.
+     */
+    start = sel4test_timestamp(env);
+    revoke_func(env->cspace_root, ep, seL4_WordBits);
+    end = sel4test_timestamp(env);
 
-    ZF_LOGD("    Creating %d caps .", (1 << (num_cnode_bits + CNODE_SIZE_BITS)));
-    for (int i = 0; i < (1 << num_cnode_bits); i++) {
-        seL4_CPtr ctable = vka_alloc_cnode_object_leaky(&env->vka, CNODE_SIZE_BITS);
+    test_geq(end, start);
 
-        for (int j = 0; j < (1 << CNODE_SIZE_BITS); j++) {
-            error = seL4_CNode_Copy(
-                        ctable, j, CNODE_SIZE_BITS,
-                        env->cspace_root, ep, seL4_WordBits,
-                        seL4_AllRights);
+    diff = end - start;
 
-            test_assert(!error);
-            num_caps++;
-        }
-        ZF_LOGD(".");
-    }
-    test_check(num_caps > 0);
-    test_check((num_caps == (1 << (num_cnode_bits + CNODE_SIZE_BITS))));
+    /* Set a timeout value to half of the revoke operation time to allow
+     * preemption in the middle, as the test expects.
+     */
+    timeout_val = diff / 2;
 
-    sel4test_periodic_start(env, NS_IN_MS);
+    /* Now, actually create a ctable that is gonna be used in the revoke
+     * test.
+     */
+    error = create_cnode_table(env, num_cnode_bits, ep);
+    test_assert(!error);
+    sel4test_periodic_start(env, timeout_val);
 
     /* Last thread to start runs first. */
     revoking = 0;
@@ -103,8 +145,16 @@ static int
 test_preempt_revoke(env_t env)
 {
     for (int num_cnode_bits = 1; num_cnode_bits < 32; num_cnode_bits++) {
-        if (test_preempt_revoke_actual(env, num_cnode_bits) > 1) {
+        int result = test_preempt_revoke_actual(env, num_cnode_bits);
+        if (result > 1) {
             return sel4test_get_result();
+        } else if (result == -1) {
+            /* At this point we assume this error is a problem where the time of
+             *  revoc_func < timer_resolution, which will cause set timeout to faule
+             *  return an error. Skip such small cnodes and only work with cnode sizes where
+             *  revoc_func > timer_resolution
+             */
+            continue;
         }
     }
 
