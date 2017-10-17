cd5a9f1d94e0363e6b90689273b49c845ba4f6fe
Hesham Almatary <hesham.almatary@data61.csiro.au>
2017-10-18 09:54:35 +1100

    SELFOUR-1146: Add test to check that ltimer moves between gettime calls

--- apps/sel4test-driver/src/tests/timer.c
+++ apps/sel4test-driver/src/tests/timer.c
@@ -34,3 +34,29 @@ test_timer(driver_env_t env)
     return sel4test_get_result();
 }
 DEFINE_TEST_BOOTSTRAP(TIMER0001, "Basic timer testing", test_timer, config_set(CONFIG_HAVE_TIMER))
+
+int
+test_gettime_timeout(driver_env_t env)
+{
+    int error = 0;
+    uint64_t start, end;
+
+    start = timestamp(env);
+    error = ltimer_set_timeout(&env->timer.ltimer, 1 * NS_IN_MS, TIMEOUT_PERIODIC);
+    test_assert_fatal(!error);
+
+    for (int i = 0; i < 3; i++) {
+        wait_for_timer_interrupt(env);
+        ZF_LOGV("Tick\n");
+    }
+
+    end = timestamp(env);
+
+    test_ge(end, start);
+
+    error = ltimer_reset(&env->timer.ltimer);
+    test_assert_fatal(!error);
+
+    return sel4test_get_result();
+}
+DEFINE_TEST_BOOTSTRAP(TIMER0002, "Test that the timer moves between gettime and timeout calls", test_gettime_timeout, config_set(CONFIG_HAVE_TIMER))
