0e48c737ef9bab98d71e25c5ad6190eed8c97e05
Adrian Danis <Adrian.Danis@data61.csiro.au>
2018-02-19 16:22:07 +1100

    Use simple wrapper for TSC frequency
    The archInfo word of bootinfo has been removed and so we use this wrapper instead, which
    will retrieve the TSC frequency from the extended bootinfo

--- apps/sel4test-driver/src/plat/pc99/plat.c
+++ apps/sel4test-driver/src/plat/pc99/plat.c
@@ -12,10 +12,11 @@
 
 #include "../../test.h"
 
-#include <sel4platsupport/bootinfo.h>
+#include <sel4utils/arch/tsc.h>
 
 void
 plat_init(driver_env_t env)
 {
-    env->init->tsc_freq = platsupport_get_bootinfo()->archInfo;
+    env->init->tsc_freq = x86_get_tsc_freq_from_simple(&env->simple);
+    ZF_LOGF_IF(env->init->tsc_freq == 0, "Failed get TSC frequency");
 }
