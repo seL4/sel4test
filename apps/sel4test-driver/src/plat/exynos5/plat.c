9cb49fbf717567aa553bdcec902bd31dd99462c3
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-11-26 16:13:35 +1100

    sel4test-driver: plat_init for exynos5
    Implemented platform initialisation for exynos5. This
    specifically sets the clock frequency to a consistent value that
    our serial driver expects.

---
+++ apps/sel4test-driver/src/plat/exynos5/plat.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ */
+#include "../../test.h"
+
+#include <platsupport/plat/clock.h>
+#include <sel4platsupport/io.h>
+#include <utils/zf_log.h>
+
+#if defined(CONFIG_PLAT_EXYNOS5422)
+void
+plat_init(driver_env_t env)
+{
+    int error;
+    ps_io_ops_t io_ops = {};
+    clock_sys_t clock = {};
+    clk_t *clk;
+
+    error = sel4platsupport_new_io_mapper(env->vspace, env->vka, &io_ops.io_mapper);
+    if (error != 0) {
+        ZF_LOGF("Failed to initialise IO mapper");
+    }
+
+    error = clock_sys_init(&io_ops, &clock);
+    if (error != 0) {
+        ZF_LOGF("Failed to initalise clock");
+    }
+
+    clk = clk_get_clock(&clock, CLK_UART2);
+    if (clk == NULL) {
+        ZF_LOGF("Failed to get clock");
+    }
+
+    clk_set_freq(clk, UART_DEFAULT_FIN);
+}
+#endif
