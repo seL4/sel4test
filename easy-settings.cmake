96e0778c966106aa32414755f6eaa98500486bf1
G. Branden Robinson <Branden.Robinson@data61.csiro.au>
2019-04-08 17:18:32 +1000

    Add griddle support to sel4test
    Migrate "basic" settings from CMakeLists.txt to easy-settings.cmake.

---
+++ easy-settings.cmake
@@ -0,0 +1,35 @@
+#
+# Copyright 2019, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+# Define our top level settings.  Whilst they have doc strings for readability
+# here, they are hidden in the cmake-gui as they cannot be reliably changed
+# after the initial configuration.  Enterprising users can still change them if
+# they know what they are doing through advanced mode.
+#
+# Users should initialize a build directory by doing something like:
+#
+# mkdir build_sabre
+# cd build_sabre
+#
+# Then
+#
+# ../griddle --PLATFORM=sabre --SIMULATION
+# ninja
+#
+set(SIMULATION OFF CACHE BOOL "Include only simulation compatible tests")
+set(RELEASE OFF CACHE BOOL "Performance optimized build")
+set(VERIFICATION OFF CACHE BOOL "Only verification friendly kernel features")
+set(BAMBOO OFF CACHE BOOL "Enable machine parseable output")
+set(DOMAINS OFF CACHE BOOL "Test multiple domains")
+set(SMP OFF CACHE BOOL "(if supported) Test SMP kernel")
+set(PLATFORM "x86_64" CACHE STRING "Platform to test")
+set(ARM_HYP OFF CACHE BOOL "Hyp mode for ARM platforms")
