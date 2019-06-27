d5d8affef6be5b371fab0b9d997125535d3b25b3
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-06-28 09:24:37 +1000

    settings.cmake: Refactor for seL4Config.cmake
    We can now import kernel platform settings within settings.cmake. This
    makes it easier to enumerate different platform options and also
    provides flags that remove the need to do string comparisons.

--- settings.cmake
+++ settings.cmake
@@ -12,26 +12,48 @@
 
 cmake_minimum_required(VERSION 3.7.2)
 
-include(${CMAKE_SOURCE_DIR}/tools/seL4/cmake-tool/helpers/application_settings.cmake)
+set(project_dir "${CMAKE_CURRENT_LIST_DIR}")
+get_filename_component(resolved_path ${CMAKE_CURRENT_LIST_FILE} REALPATH)
+# repo_dir is distinct from project_dir as this file is symlinked.
+# project_dir corresponds to the top level project directory, and
+# repo_dir is the absolute path after following the symlink.
+get_filename_component(repo_dir ${resolved_path} DIRECTORY)
 
+include(${project_dir}/tools/seL4/cmake-tool/helpers/application_settings.cmake)
 # Set our custom domain schedule
-set(KernelDomainSchedule "${CMAKE_CURRENT_LIST_DIR}/domain_schedule.c" CACHE INTERNAL "")
+set(KernelDomainSchedule "${repo_dir}/domain_schedule.c" CACHE INTERNAL "")
 
 # Declare a cache variable that enables/disablings the forcing of cache variables to
 # the specific test values. By default it is disabled
 set(Sel4testAllowSettingsOverride OFF CACHE BOOL "Allow user to override configuration settings")
 
-include(easy-settings.cmake)
+include(${repo_dir}/easy-settings.cmake)
 
-# We temporarily remove this circular dependency as this file will be processed before the kernel files that define _all_strings
-# set_property(
-#     CACHE PLATFORM
-#     PROPERTY
-#         STRINGS
-#         ${KernelX86Sel4Arch_all_strings}
-#         ${KernelARMPlatform_all_strings}
-#         ${KernelRiscVPlatform_all_strings}
-# )
+correct_platform_strings()
+
+include(${project_dir}/kernel/configs/seL4Config.cmake)
+
+set(
+    valid_platforms
+    ${KernelPlatform_all_strings}
+    sabre
+    wandq
+    kzm
+    rpi3
+    exynos5250
+    exynos5410
+    exynos5422
+    am335x-boneblack
+    am335x-boneblue
+    x86_64
+    ia32
+)
+set_property(CACHE PLATFORM PROPERTY STRINGS ${valid_platforms})
+list(FIND valid_platforms "${PLATFORM}" index)
+if("${index}" STREQUAL "-1")
+    message(FATAL_ERROR "Invalid PLATFORM selected: \"${PLATFORM}\"
+Valid platforms are: \"${valid_platforms}\"")
+endif()
 
 mark_as_advanced(
     CLEAR
@@ -42,43 +64,18 @@ mark_as_advanced(
 # We use 'FORCE' when settings these values instead of 'INTERNAL' so that they still appear
 # in the cmake-gui to prevent excessively confusing users
 if(NOT Sel4testAllowSettingsOverride)
-    # Determine the platform/architecture
-    if(RISCV64 OR RISCV32)
-        set(KernelArch riscv CACHE STRING "" FORCE)
-        set(KernelRiscVPlatform ${PLATFORM} CACHE STRING "" FORCE)
-        if(RISCV64)
-            set(KernelRiscVSel4Arch "riscv64" CACHE STRING "" FORCE)
-        else()
-            set(KernelRiscVSel4Arch "riscv32" CACHE STRING "" FORCE)
-        endif()
-    elseif(AARCH32 OR AARCH64 OR ARM_HYP OR ARM OR AARCH32HF)
-        set(KernelArch arm CACHE STRING "" FORCE)
-        set(KernelARMPlatform ${PLATFORM} CACHE STRING "" FORCE)
-        if(ARM_HYP)
-            set(KernelArmHypervisorSupport ON CACHE BOOL "" FORCE)
-        endif()
-
-        if(AARCH64)
-            set(KernelArmSel4Arch "aarch64" CACHE STRING "" FORCE)
-        elseif(AARCH32 OR ARM OR AARCH32HF)
-            set(KernelArmSel4Arch "aarch32" CACHE STRING "" FORCE)
-            if(ARM_HYP)
-                set(KernelArmSel4Arch "arm_hyp" CACHE STRING "" FORCE)
-            endif()
-        endif()
-
+    if(ARM_HYP)
+        set(KernelArmHypervisorSupport ON CACHE BOOL "" FORCE)
+    endif()
+    if(KernelArchARM)
         # Elfloader settings that correspond to how Data61 sets its boards up.
-        ApplyData61ElfLoaderSettings(${KernelARMPlatform} ${KernelArmSel4Arch})
-
-    else()
-        set(KernelArch x86 CACHE STRING "" FORCE)
-        set(KernelX86Sel4Arch ${PLATFORM} CACHE STRING "" FORCE)
+        ApplyData61ElfLoaderSettings(${KernelARMPlatform} ${KernelSel4Arch})
     endif()
 
     if(SIMULATION)
         ApplyCommonSimulationSettings(${KernelArch})
     else()
-        if("${KernelArch}" STREQUAL "x86")
+        if(KernelArchX86)
             set(KernelIOMMU ON CACHE BOOL "" FORCE)
         endif()
     endif()
@@ -93,7 +90,7 @@ if(NOT Sel4testAllowSettingsOverride)
     else()
         set(Sel4testHaveCache ON CACHE BOOL "" FORCE)
     endif()
-    if(("${KernelArch}" STREQUAL "riscv") OR (SIMULATION AND ("${KernelArch}" STREQUAL "arm")))
+    if(KernelArchRiscV OR (SIMULATION AND KernelArchARM))
         set(Sel4testHaveTimer OFF CACHE BOOL "" FORCE)
     else()
         set(Sel4testHaveTimer ON CACHE BOOL "" FORCE)
@@ -103,15 +100,14 @@ if(NOT Sel4testAllowSettingsOverride)
     # skipping any aarch64 platform, as this does not yet support the debug API, and a
     # few other miscelaneous platforms that do not support it
     if(
-        ((NOT SIMULATION) OR ("${KernelX86Sel4Arch}" STREQUAL "ia32"))
-        AND (NOT ("${KernelArmSel4Arch}" STREQUAL "aarch64"))
-        AND (NOT ("${KernelArch}" STREQUAL "riscv"))
-        AND (NOT ("${KernelARMPlatform}" STREQUAL "exynos5250"))
-        AND (NOT ("${KernelARMPlatform}" STREQUAL "am335x-boneblack"))
-        AND (NOT ("${KernelARMPlatform}" STREQUAL "am335x-boneblue"))
-        AND (NOT ("${KernelARMPlatform}" STREQUAL "omap3"))
-        AND (NOT ("${KernelARMPlatform}" STREQUAL "kzm"))
-        AND (NOT ("${KernelARMPlatform}" STREQUAL "exynos5410"))
+        ((NOT SIMULATION) OR KernelSel4ArchIA32)
+        AND (NOT KernelSel4ArchAarch64)
+        AND (NOT KernelArchRiscV)
+        AND (NOT KernelPlatformExynos5250)
+        AND (NOT KernelPlatformAM335X)
+        AND (NOT KernelPlatformOMAP3)
+        AND (NOT KernelPlatformKZM)
+        AND (NOT KernelPlatformExynos5410)
     )
         set(HardwareDebugAPI ON CACHE BOOL "" FORCE)
     else()
