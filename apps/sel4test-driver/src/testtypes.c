77206eb2f4525fae3a746403495e0770a2aed448
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2019-07-15 08:01:12 +1000

    sel4test-driver: Remove custom rpc wrapper
    We don't require a custom allocation implementation now as it is
    sufficient to allocate via simple and vka interfaces.

--- apps/sel4test-driver/src/testtypes.c
+++ apps/sel4test-driver/src/testtypes.c
@@ -18,7 +18,7 @@
 
 #include "test.h"
 #include "timer.h"
-#include "rpc.h"
+#include <sel4rpc/server.h>
 #include <sel4testsupport/testreporter.h>
 
 /* Bootstrap test type. */
@@ -124,7 +124,7 @@ static int sel4test_driver_wait(driver_env_t env, struct testcase *test)
     seL4_Word badge = 0;
     sel4rpc_server_env_t rpc_server;
 
-    sel4rpc_server_init(&rpc_server, &env->vka, sel4test_rpc_recv, env,
+    sel4rpc_server_init(&rpc_server, &env->vka, sel4rpc_default_handler, env,
                         &env->reply, &env->simple);
 
     while (1) {
