ae6931e1777d7997afd9f0d57b8f8d7b8d4f0dc1
Curtis Millar <curtis.millar@data61.csiro.au>
2021-05-13 10:10:09 +1000

    trivial: Remove comment that no longer applies
    Signed-off-by: Curtis Millar <curtis.millar@data61.csiro.au>

--- apps/sel4test-tests/src/tests/faults.c
+++ apps/sel4test-tests/src/tests/faults.c
@@ -1057,7 +1057,6 @@ static int test_timeout_fault_nested_servers(env_t env)
 
     return sel4test_get_result();
 }
-/* this test is disabled for the same reason as TIMEOUTFAULT0002 */
 DEFINE_TEST(TIMEOUTFAULT0003, "Nested timeout fault", test_timeout_fault_nested_servers, config_set(CONFIG_KERNEL_MCS))
 
 static void vm_enter(void)
