fae7635182b4d61c487b769e95a552ffecaa3a3f
Kent McLeod <kent@kry10.com>
2021-12-08 10:32:04 +1100

    scheduler: Enable SCHED0000 on riscv
    This test isn't arch specific and so is expected to pass on riscv.
    
    Signed-off-by: Kent McLeod <kent@kry10.com>

--- apps/sel4test-tests/src/tests/scheduler.c
+++ apps/sel4test-tests/src/tests/scheduler.c
@@ -98,8 +98,7 @@ static int test_thread_suspend(env_t env)
     return sel4test_get_result();
 }
 DEFINE_TEST(SCHED0000, "Test suspending and resuming a thread (flaky)", test_thread_suspend,
-            config_set(CONFIG_HAVE_TIMER)
-            && !config_set(CONFIG_ARCH_RISCV))
+            config_set(CONFIG_HAVE_TIMER))
 
 /*
  * Test TCB Resume on self.
