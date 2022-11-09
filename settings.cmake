eeb49218fa560e91c66fde084d932f56f1b60d62
Axel Heider <axel.heider@hensoldt.net>
2022-11-09 15:38:12 +0100

    Set SIMULATION for QEMU RISC-V virt by default
    Signed-off-by: Axel Heider <axel.heider@hensoldt.net>

--- settings.cmake
+++ settings.cmake
@@ -48,7 +48,7 @@ if(NOT Sel4testAllowSettingsOverride)
         set(KernelArmHypervisorSupport ON CACHE BOOL "" FORCE)
     endif()
 
-    if(KernelPlatformQEMUArmVirt OR KernelPlatformSpike)
+    if(KernelPlatformQEMUArmVirt OR KernelPlatformQEMURiscVVirt OR KernelPlatformSpike)
         set(SIMULATION ON CACHE BOOL "" FORCE)
     endif()
 
